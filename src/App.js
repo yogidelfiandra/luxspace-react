f8528ab1f36d0538df4299cc6faa15461715cb7a
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2021-05-06 22:54:57 +0700

    complete homepage
    -create file env
    -update file App
    -smart component: Carousel
    -custom hooks: useAsync
    -custom fetch API
    -skeleton Loading
    -update Browse room
    -update Just Arrived
    -update Sitemap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Congratulation from "pages/Congratulation";
 import Details from "pages/Details";
 import HomePage from "pages/HomePage";
 import NotFound from "pages/NotFound";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/css/app.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/categories/:idc" component={Details} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/congratulation" component={Congratulation} />
-        <Route path="*" component={NotFound} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/categories/:idc" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/congratulation" component={Congratulation} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </Router>
     </div>
   );
